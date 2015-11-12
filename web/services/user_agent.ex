c7d5ce56398aae1982baad58fcbaf1ff49a04058
Joshua Clayton <jclayton@thoughtbot.com>
2015-11-11 23:06:59 -0500

    Pattern match on generic structs

--- web/services/user_agent.ex
+++ web/services/user_agent.ex
@@ -1,22 +1,22 @@
 defmodule RedirectTo.UserAgent do
   def browser_name(user_agent) do
     case UaInspector.parse(user_agent) do
-      %UaInspector.Result{client: %UaInspector.Result.Client{name: name}} -> name
+      %{client: %{name: name}} -> name
       _ -> "Unknown"
     end
   end
 
   def os_name(user_agent) do
     case UaInspector.parse(user_agent) do
-      %UaInspector.Result{os: %UaInspector.Result.OS{name: name}} -> name
+      %{os: %{name: name}} -> name
       _ -> "Unknown"
     end
   end
 
   def device_name(user_agent) do
     case UaInspector.parse(user_agent) do
-      %UaInspector.Result{device: %UaInspector.Result.Device{model: :unknown}} -> "Unknown"
-      %UaInspector.Result{device: %UaInspector.Result.Device{model: name}} -> name
+      %{device: %{model: :unknown}} -> "Unknown"
+      %{device: %{model: name}} -> name
       _ -> "Unknown"
     end
   end
