a33eaa718bc038317ceef7774899c3dab3fbf7fb
Joshua Clayton <jclayton@thoughtbot.com>
2015-11-11 20:49:01 -0500

    Omit port from config when deploying to Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 
 config :redirect_to, RedirectTo.Endpoint,
   url: [host: System.get_env("HOST")],
-  http: [port: System.get_env("PORT"), compress: true],
+  http: [compress: true],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
