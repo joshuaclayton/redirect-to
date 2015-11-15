8e54d1d7b3305b72034d98f9749dba60a5f32fbb
Joshua Clayton <jclayton@thoughtbot.com>
2015-11-15 00:21:11 -0500

    Remove unused function

--- web/views/link_view.ex
+++ web/views/link_view.ex
@@ -1,10 +1,6 @@
 defmodule RedirectTo.LinkView do
   use RedirectTo.Web, :view
 
-  def visits_count(link) do
-    link.link_visits |> length
-  end
-
   def flag_active(current_link, active_link) do
     if active_link && active_link.id == current_link.id do
       "class=active"
