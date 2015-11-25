8ea3faa9fc2ba7863eb8210d87281f57375a8d3d
Joshua Clayton <jclayton@thoughtbot.com>
2015-11-24 21:05:28 -0500

    Remove unused variable

--- priv/repo/prime.exs
+++ priv/repo/prime.exs
@@ -1,6 +1,6 @@
 RedirectTo.Repo.delete_all RedirectTo.LinkVisit
 RedirectTo.Repo.delete_all RedirectTo.Link
 
-1..52 |> Enum.each fn(x) ->
+1..52 |> Enum.each fn(_) ->
   RedirectTo.LinkCreator.create %{long_url: "http://joshuaclayton.me"}
 end
