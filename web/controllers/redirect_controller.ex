b5b6698b03fe656e771847e36c7179efe78f472c
Joshua Clayton <joshua.clayton@gmail.com>
2016-07-05 21:37:21 -0400

    Configure link cache usage

--- web/controllers/redirect_controller.ex
+++ web/controllers/redirect_controller.ex
@@ -4,9 +4,13 @@ defmodule RedirectTo.RedirectController do
   alias RedirectTo.Queries
 
   def show(conn, %{"slug" => slug}) do
-    link = LinkCache.fetch(slug, fn ->
-      Queries.Link.by_slug(slug) |> Repo.one
-    end)
+    link = case System.get_env("USE_LINK_CACHE") do
+      "true" ->
+        LinkCache.fetch(slug, fn ->
+          Queries.Link.by_slug(slug) |> Repo.one
+        end)
+      _ -> Queries.Link.by_slug(slug) |> Repo.one
+    end
 
     spawn_link(LinkVisitCreator, :create, [conn, link])
 
